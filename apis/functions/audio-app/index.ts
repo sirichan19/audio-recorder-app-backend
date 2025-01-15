660c25e92393982d28b2790cee2f49b3f3cdc7ff
sirichan19 <siri.chandana2020@gmail.com>
2025-01-16 00:36:45 +0530

    file structure changed

---
+++ apis/functions/audio-app/index.ts
@@ -0,0 +1,144 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import * as AWS from 'aws-sdk';
+
+const s3 = new AWS.S3();
+const bucketName = process.env.BUCKET_NAME || 'audio-bucket-1234567890';
+//const MAX_CHUNKS = 5;
+let chunkStore: Buffer[] = [];
+export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+console.log('event.httpMethod--->', event.httpMethod);
+
+  switch (event.httpMethod) {
+    case 'POST':
+      return addAudioChunkOrMergeChunks(event);
+    case 'GET':
+      return retrieveAudioFromS3(event);
+    case 'DELETE':
+      return deleteAudioFromS3(event);
+    default:
+      return {
+        statusCode: 400,
+        body: JSON.stringify('Bad request.......'),
+      };
+  }
+  
+};
+
+const addAudioChunkOrMergeChunks  = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  if(event.resource.includes('add')){
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify('Invalid request, no audio found'),
+    };
+  }
+  const {chunk} = JSON.parse(event.body);
+
+  chunkStore.push(Buffer.from(chunk, 'base64'));
+
+  if(chunkStore.length > 0){
+    return {
+      statusCode: 200,
+      body: JSON.stringify('Chunk stored temporarily in Lambda'),
+      headers: {
+        "Access-Control-Allow-Origin": "*",  // Or specify the exact domain, e.g., "https://yourfrontend.com"
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
+    };
+  }
+}
+
+  if(event.resource.includes('merge')){
+    const mergedChunk = Buffer.concat(chunkStore);
+    const mergedKey = `/merged-audio/${Date.now()}-audio.mp3`;
+  try{
+    await s3.putObject({
+      Bucket: bucketName,
+      Key: mergedKey,
+      Body: mergedChunk,
+      ContentType: 'audio/mpeg'
+    }).promise();
+  
+    chunkStore = []; // clear the stored chunks
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        key : mergedKey,
+        message: 'Recording saved to S3 successfully',
+        headers: {
+          "Access-Control-Allow-Origin": "*",  // Or specify the exact domain, e.g., "https://yourfrontend.com"
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+          "Access-Control-Allow-Headers": "Content-Type"
+        },
+      })
+    };
+  }catch(err){
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify( {message : 'Recording processing failed', error : err }),
+    };
+  
+  }
+  
+}
+return {
+  statusCode: 400,
+  body: JSON.stringify('Invalid request'),
+};
+};
+
+const retrieveAudioFromS3 = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> =>{
+  const audioKey = event.queryStringParameters?.key ||  `/merged-audio/${Date.now()}-audio.mp3`;
+
+  try {
+    const data = await s3.getObject({
+      Bucket: bucketName,
+      Key: audioKey,
+    }).promise()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        key : audioKey,
+        message: 'Recording retrieved successfully',
+        audio: data.Body ? Buffer.from(data.Body as string).toString('base64') : ''
+      }),
+      headers: {
+        'Content-Type': data.ContentType || 'audio/mpeg',
+        'Content-Encoding': 'base64',
+        "Access-Control-Allow-Origin": "*",  // Or specify the exact domain, e.g., "https://yourfrontend.com"
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+          "Access-Control-Allow-Headers": "Content-Type"
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error retrieving audio from S3', error: err }),
+    };
+  }
+};
+const deleteAudioFromS3 = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult>  => {
+  const audioKey = event.queryStringParameters?.key || '/merged-audio/${Date.now()}-audio.mp3';
+
+  try {
+    await s3.deleteObject({
+      Bucket: bucketName,
+      Key: audioKey,
+    }).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify('Audio deleted successfully from S3'),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error deleting audio from S3', error: err }),
+    };
+  }
+};
